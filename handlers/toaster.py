73a927b8f85eea43b51279f96a0afe3f27f298b0
suharshs <suharshs@gmail.com>
2012-09-15 02:02:35 -0500

    added structure of toaster handler

---
+++ handlers/toaster.py
@@ -0,0 +1,8 @@
+from tornado.web import RequestHandler
+
+
+class Toaster(RequestHandler):
+	def post(self, message, drink, timestamp):
+		# store the information into redis
+
+		# return if the post was sucessful or not
