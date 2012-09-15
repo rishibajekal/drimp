5ca3913cba54dd252967b15a6f9f42c152b65bdb
suharshs <suharshs@gmail.com>
2012-09-15 02:34:32 -0500

    added the toast form in index.html

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,3 @@
 // This file holds the javascript code of the core ajax queries that update the toasts
+
+//Use the selector for the elements in the form to access the elements and send them via ajax
